7092545fc129bd8b8f73d8fbab5e2458a4671a9a
Somin <sominupadhyaya@gmail.com>
2022-08-15 21:36:53 +0545

    complete good timer

--- src/components/GoodTimer.tsx
+++ src/components/GoodTimer.tsx
@@ -2,13 +2,18 @@ import { useEffect , useState } from "react"
 
 const GoodTimer = () => {
     const [time, setTime] = useState<number>(0)
-
+    const [timerState, setTimerState] = useState<boolean>(false)
     useEffect(() =>{
-    const interval = setInterval(() =>{
+    if(!timerState){
+        let interval = setInterval(() =>{
             setTime(t => t+0.1)
         } , 100)
-    return () => clearInterval(interval)
-    } , [])
+        return () => clearInterval(interval)
+    }else{
+
+        setTime(time)
+    }
+    } , [timerState])
     
     
     return (
@@ -16,6 +21,8 @@ const GoodTimer = () => {
     <div>GoodTimer</div>
     <h1>Good Timer</h1>
     <div>{time.toFixed()}</div>
+    <button onClick={() => setTimerState(true)}>Stop Time</button>
+    <button onClick={() => setTimerState(false)}>Start Time</button>
     </>
   )
 }
